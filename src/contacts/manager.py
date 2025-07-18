bcfd288296fc0f079990f06257ce4b01e157da53
Joash Brahma <joashbrahma115@gmail.com>
2025-07-18 20:20:37 +0530

    Add add method to save a new contact; Import csv for CSV handling and Contact for type hinting

--- src/contacts/manager.py
+++ src/contacts/manager.py
@@ -1,3 +1,9 @@
+# Built-in
+import csv
+
+# My Module
+from .contact import Contact
+
 class ContactManager:
     """
     Represents a manager for a list of contacts.
@@ -5,3 +11,11 @@ class ContactManager:
     def __init__(self, csvfilename: str, fieldnames: list):
         self.csvfilename: str = csvfilename
         self.fieldnames: list = fieldnames
+
+    def add(self, contact: Contact):
+        """
+        Adds a contact to the contact list.
+        """
+        with open(self.csvfilename, "a", newline="", encoding="utf-8") as file:
+            writer = csv.DictWriter(file, self.fieldnames)
+            writer.writerow(contact.to_dict())
