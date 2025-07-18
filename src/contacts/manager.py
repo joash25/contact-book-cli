e4b944ec9ec9a1d319aa226dee75b1990cc13cd6
Joash Brahma <joashbrahma115@gmail.com>
2025-07-18 20:10:49 +0530

    Add fieldnames parameter to ContactManager constructor to support CSV

--- src/contacts/manager.py
+++ src/contacts/manager.py
@@ -2,6 +2,6 @@ class ContactManager:
     """
     Represents a manager for a list of contacts.
     """
-    def __init__(self, csvfilename: str):
+    def __init__(self, csvfilename: str, fieldnames: list):
         self.csvfilename: str = csvfilename
- 
+        self.fieldnames: list = fieldnames
